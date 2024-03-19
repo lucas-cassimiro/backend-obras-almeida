c788515d799776164a386aadaaa8e14c157d6b4d
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-03-19 17:40:18 -0300

    qualityController create

--- src/app/controllers/QualityControlController.ts
+++ src/app/controllers/QualityControlController.ts
@@ -1,7 +1,32 @@
-// import { Request, Response } from "express";
+import { Request, Response } from "express";
 
-// import prisma from "config/clientPrisma";
+import prisma from "config/clientPrisma";
+
+type RequestBodyItem = {
+  id: number;
+  resultado: string;
+  acao: string;
+};
 
 export class QualityControlController {
-   
+    async create(req: Request, res: Response) {
+        try {
+            const { body } = req;
+
+            for (const { id, resultado, acao } of body as RequestBodyItem[]) {
+                await prisma.qualityControl.create({
+                    data: {
+                        productivity_id: id,
+                        nonconformity: resultado,
+                        operation: acao,
+                    },
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            return res
+                .status(500)
+                .send({ message: "Não foi possível cadastrar os dados." });
+        }
+    }
 }
